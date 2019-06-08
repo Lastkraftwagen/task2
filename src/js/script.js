b69a1ac7396f4bdb64a4f9958fcfb89b5cd06449
Jagermeister <miriladon@gmail.com>
2019-06-08 23:38:32 +0300

    поделал немного

--- src/js/script.js
+++ src/js/script.js
@@ -38,11 +38,54 @@ const loadAll = () => {
             for (let i = 0; i < temp.length; i++) {
                 temp[i].addEventListener('click', OnCategoryClick);
             }
+            categoryClick(data,temp[0].id,temp[0].innerHTML)
         })
 }
 
 document.addEventListener('loadend', loadAll())
 
+function categoryClick(data, id, text)
+{
+        let selected_cat;
+        const items = [];
+        let el = document.getElementById("category_name").firstElementChild;
+        el.innerHTML = text;
+        selected_cat = data.categories
+            .find((element, index, array) => {
+                if (element.id == id)
+                    return true;
+                return false;
+            });
+        data.items.forEach(element => {
+            if (selected_cat.items.includes(element.id)) {
+                items.push(element)
+            }
+        });
+        el = document.querySelector("#items_holder");
+        console.log(el.childNodes);
+        console.log(items);
+        removeElements( document.querySelectorAll(".item") );
+        items.sort((a, b) => {
+            if (a.position < b.position) return -1;
+            if (a.position > b.position) return 1;
+            return 0;
+        });
+        let counter = 0;
+        
+        items.forEach(element => {
+            let node = document.createElement("div");
+            if (counter == 0) node.className = "item selected";
+            else node.className = "item";
+            node.addEventListener('click', OnItemClick);
+            node.setAttribute("id", element.id);
+            node.setAttribute("title", element.title);
+            node.setAttribute("description", element.description);
+            node.setAttribute("long_description", element.long_description);
+            counter++;
+            node.innerHTML = element.title;
+            el.appendChild(node);
+        });
+    }
 
 
 function OnCategoryClick(e) {
@@ -57,48 +100,16 @@ function OnCategoryClick(e) {
             this.data = json;
             return data;
         })
-        .then(data => {
+        .then(data=>{
             let id = e.srcElement.id;
             let text = e.srcElement.innerHTML;
-            let selected_cat;
-            const items = [];
-            let el = document.getElementById("category_name").firstElementChild;
-            el.innerHTML = text;
-            selected_cat = data.categories
-                .find((element, index, array) => {
-                    if (element.id == id)
-                        return true;
-                    return false;
-                });
-            data.items.forEach(element => {
-                if (selected_cat.items.includes(element.id)) {
-                    items.push(element)
-                }
-            });
-            el = document.querySelector("#items_holder");
-            console.log(el);
-            console.log(items);
-
-            el.childNodes.forEach(element => {
-                if (element.className == "item" || 
-                    element.className == "item selected") {
-                    el.removeChild(element);
-                }
-            });
-            items.sort((a, b) => {
-                if (a.position < b.position) return -1;
-                if (a.position > b.position) return 1;
-                return 0;
-            });
-            let counter = 0;
-            
-            items.forEach(element => {
-                let node = document.createElement("div");
-                if (counter == 0) node.className = "item selected";
-                else node.className = "item";
-                counter++;
-                node.innerHTML = element.title;
-                el.appendChild(node);
-            });
+            categoryClick(data,id,text);
         });
 }
+
+function OnItemClick(e) {
+    console.log(e.srcElement);
+    
+}
+
+const removeElements = (elms) => elms.forEach(el => el.remove());
